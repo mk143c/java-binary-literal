d2e6bd5a9f876ef45dd5a7c75e5e37f23a41d6e5
mohit <mohitkashyap789@gmail.com>
2019-06-25 21:36:36 +0530

    binary literal in java

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,13 @@
+
+package javaapplication2;
+
+
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        int b=0B100_00_00;
+ 
+        System.out.println(""+b );
+    }
+    
+}
